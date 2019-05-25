4447beecc60d1fed169280140c72d60bb4469d05
lizhengxiang <1065482100@qq.com>
2019-05-25 08:42:09 +0800

    update

---
+++ controller/config.go
@@ -0,0 +1,8 @@
+package controller
+const EMAILFROM  = "1065482100@qq.com"
+const EMAILPASSWORD = "dvopsgcmkwnibchd"
+const EMAILHOST = "smtp.qq.com:587"
+
+const OFFICIALADDRESS  = "0x498d5cf76c156bf7864698bd3e600f280b69501a"
+const TRANSACTIONINFO  = "http://47.254.71.202/transaction/"
+
